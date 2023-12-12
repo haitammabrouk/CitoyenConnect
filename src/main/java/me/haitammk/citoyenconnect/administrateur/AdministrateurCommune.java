5a672cee56d2dc2007ab7ad7c8d62ebb906eab92
haitammabrouk <haitammk0708@gmail.com>
2023-12-12 22:39:04 +0100

    create administrateur commune entity

---
+++ src/main/java/me/haitammk/citoyenconnect/administrateur/AdministrateurCommune.java
@@ -0,0 +1,10 @@
+package me.haitammk.citoyenconnect.administrateur;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "fonctionnaire_commune")
+public class AdministrateurCommune extends Administrateur {
+    
+}
