e22cc10bae003cf8913a5aef03e80b4cd725a3c3
haitammabrouk <haitammk0708@gmail.com>
2023-12-12 22:16:20 +0100

    create personne entity

---
+++ src/main/java/me/haitammk/citoyenconnect/personne/Personne.java
@@ -0,0 +1,81 @@
+package me.haitammk.citoyenconnect.personne;
+
+import java.sql.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "personne")
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+public class Personne {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String cin;
+
+    @NotBlank
+    @Column(name = "nom")
+    private String nom;
+    
+    @NotBlank
+    @Column(name = "prenom")
+    private String prenom;
+
+    @NotBlank
+    @Column(name = "email", unique = true)
+    private String email;
+    
+    @NotBlank
+    @Size(min = 8, message = "Password must be at least characters long")
+    @Column(name = "password")
+    private String password;
+
+    @NotBlank
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "token", unique = true)
+    private String token;
+
+    @Column(name = "expiry_token")
+    private Date expiryToken;
+
+    @NotBlank
+    @Column(name = "lieu_naissance")
+    private String lieuNaissance;
+
+    @NotBlank
+    @Column(name = "date_naissance")
+    private Date dateNaissance;
+
+    @NotBlank
+    @Column(name = "nationalite")
+    private String nationalite;
+
+    @Lob
+    @Column(name = "image", columnDefinition = "BLOB")
+    private byte[] image;
+
+    @Lob
+    @Column(name = "carte_nationale", columnDefinition = "BLOB")
+    private byte[] carteNationale;
+}
+
