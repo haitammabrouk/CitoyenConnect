a056237e81c1458493e00bfbe7a48cc16fe15a82
haitammabrouk <haitammk0708@gmail.com>
2023-12-12 22:59:01 +0100

    create demande egalisation entity

---
+++ src/main/java/me/haitammk/citoyenconnect/demande/DemandeEgalisation.java
@@ -0,0 +1,23 @@
+package me.haitammk.citoyenconnect.demande;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "demande_egalisation")
+public class DemandeEgalisation extends Demande {
+    
+    @Lob
+    @Column(name= "document", columnDefinition = "BLOB")
+    private byte[] document;
+}
