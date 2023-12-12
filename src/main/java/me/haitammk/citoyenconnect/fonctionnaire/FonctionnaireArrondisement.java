f4179667736adc0038b2e1ad2b23fd0eb0bd22e0
haitammabrouk <haitammk0708@gmail.com>
2023-12-12 22:23:11 +0100

    create fonctionnaire arrondisement

---
+++ src/main/java/me/haitammk/citoyenconnect/fonctionnaire/FonctionnaireArrondisement.java
@@ -0,0 +1,10 @@
+package me.haitammk.citoyenconnect.fonctionnaire;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "fonctionnaire_arrondisement")
+public class FonctionnaireArrondisement extends Fonctionnaire {
+    
+}
