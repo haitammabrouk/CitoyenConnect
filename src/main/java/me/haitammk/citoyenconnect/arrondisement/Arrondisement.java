126d890dd86a3dbecd0695dd58d0d9c924a7aa08
haitammabrouk <haitammk0708@gmail.com>
2023-12-13 22:12:01 +0100

    establish relatioship between arrondisement and fonctionnaires Arrondisement

--- src/main/java/me/haitammk/citoyenconnect/arrondisement/Arrondisement.java
+++ src/main/java/me/haitammk/citoyenconnect/arrondisement/Arrondisement.java
@@ -1,5 +1,6 @@
 package me.haitammk.citoyenconnect.arrondisement;
 
+import java.util.List;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -8,6 +9,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -16,6 +18,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import me.haitammk.citoyenconnect.administrateur.AdministrateurArrondisement;
 import me.haitammk.citoyenconnect.commune.Commune;
+import me.haitammk.citoyenconnect.fonctionnaire.FonctionnaireArrondisement;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -42,4 +45,7 @@ public class Arrondisement {
 
     @OneToOne(mappedBy = "arrondisement", cascade = CascadeType.ALL)
     private AdministrateurArrondisement administrateurArrondisement;
+
+    @OneToMany(mappedBy = "arrondisement")
+    private List<FonctionnaireArrondisement> fonctionnairesArrondisemnent;
 }
--- src/main/java/me/haitammk/citoyenconnect/fonctionnaire/FonctionnaireArrondisement.java
+++ src/main/java/me/haitammk/citoyenconnect/fonctionnaire/FonctionnaireArrondisement.java
@@ -1,10 +1,16 @@
 package me.haitammk.citoyenconnect.fonctionnaire;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import me.haitammk.citoyenconnect.arrondisement.Arrondisement;
 
 @Entity
 @Table(name = "fonctionnaire_arrondisement")
 public class FonctionnaireArrondisement extends Fonctionnaire {
     
+    @ManyToOne
+    @JoinColumn(referencedColumnName = "id_arrondisement", name = "id_arrondisement")
+    private Arrondisement arrondisement;
 }
