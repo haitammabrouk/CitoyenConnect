66a4158aec71c915522992dbc9c12938b7b1723f
haitammabrouk <haitammk0708@gmail.com>
2023-12-13 22:04:36 +0100

    establish relatioship between commune and administrateurCommune

--- src/main/java/me/haitammk/citoyenconnect/administrateur/AdministrateurCommune.java
+++ src/main/java/me/haitammk/citoyenconnect/administrateur/AdministrateurCommune.java
@@ -1,10 +1,16 @@
 package me.haitammk.citoyenconnect.administrateur;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import me.haitammk.citoyenconnect.commune.Commune;
 
 @Entity
 @Table(name = "administrateur_commune")
 public class AdministrateurCommune extends Administrateur {
     
+    @OneToOne
+    @JoinColumn(referencedColumnName = "id_commune", name = "id_commune")
+    private Commune commune;
 }
--- src/main/java/me/haitammk/citoyenconnect/commune/Commune.java
+++ src/main/java/me/haitammk/citoyenconnect/commune/Commune.java
@@ -2,17 +2,20 @@ package me.haitammk.citoyenconnect.commune;
 
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import me.haitammk.citoyenconnect.administrateur.AdministrateurCommune;
 import me.haitammk.citoyenconnect.arrondisement.Arrondisement;
 
 @NoArgsConstructor
@@ -36,4 +39,7 @@ public class Commune {
 
     @OneToMany(mappedBy = "commune")
     private List<Arrondisement> arrondisements;
+
+    @OneToOne(mappedBy = "commune", cascade = CascadeType.ALL)
+    private AdministrateurCommune administrateurCommune;
 }
