0938afcfe243401d6a1a59b15d488b812e862478
haitammabrouk <haitammk0708@gmail.com>
2023-12-14 23:46:48 +0100

    create the footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+
+function Footer() {
+  return (
+    <div className="w-full font-cairo">
+        <div className="content">
+            <div className="first-part">
+                <h3><span></span><span>Informations pratiques</span></h3>
+                <ul>
+                    <li><a href="#">A propos de CitoyenConnect</a></li>
+                    <li><a href="#">A qui s'adresser ?</a></li>
+                    <li><a href="#">Aide</a></li>
+                </ul>
+            </div>
+            <div className="second-part">
+                <h3><span></span><span>Demander D'inscrire</span></h3>
+                <ul>
+                    <li><a href="#">Créer votre Compte</a></li>
+                    <li><a href="#">Suivre ou compléter votre demande</a></li>
+                </ul>
+            </div>
+            <div className="third-part"></div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
