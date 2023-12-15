3b5a0cb04e007b16a532e9ebded38f8ac5303c7f
haitammabrouk <haitammk0708@gmail.com>
2023-12-15 19:12:29 +0100

    create recuperation par email page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import CreationMdp from './pages/CreationMdp'
+import EnterEmail from './pages/EnterEmail'
 import Login from './pages/Login'
 
 function App() {
 
   return (
     <>
-    <Login />
+    <EnterEmail />
     </>
   )
 }
