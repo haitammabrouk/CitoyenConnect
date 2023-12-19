297bde353a501f85b4f1d1c1dcda40dc77e44b9e
haitammabrouk <haitammk0708@gmail.com>
2023-12-19 12:46:13 +0100

    set the relatioship between administrateur and arrondisement

--- src/main/java/me/haitammk/citoyenconnect/administrateur/Administrateur.java
+++ src/main/java/me/haitammk/citoyenconnect/administrateur/Administrateur.java
@@ -1,11 +1,18 @@
 package me.haitammk.citoyenconnect.administrateur;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import me.haitammk.citoyenconnect.arrondisement.Arrondisement;
 import me.haitammk.citoyenconnect.personne.Personne;
 
 @Table(name = "administrateur")
 @Entity
 public class Administrateur extends Personne {
     
+    @OneToOne
+    @JoinColumn(referencedColumnName = "id_arrondisement")
+    private Arrondisement arrondisement;
+
 }
--- src/main/java/me/haitammk/citoyenconnect/arrondisement/Arrondisement.java
+++ src/main/java/me/haitammk/citoyenconnect/arrondisement/Arrondisement.java
@@ -1,16 +1,19 @@
 package me.haitammk.citoyenconnect.arrondisement;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import me.haitammk.citoyenconnect.administrateur.Administrateur;
 
 @Entity
 @Table(name = "arrondisement")
@@ -25,6 +28,9 @@ public class Arrondisement {
     @Column(name = "id_arrondisement")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
+
+    @OneToOne(mappedBy = "arrondisement", cascade = CascadeType.ALL)
+    private Administrateur administrateur;
 
     @Column(name = "adresse")
     private String adresse;
