178db8742ca8abfa3d7a0f8f0abe27bd82f28422
haitammabrouk <haitammk0708@gmail.com>
2023-12-19 12:10:37 +0100

    set the whole reclamation class

--- src/main/java/me/haitammk/citoyenconnect/reclamation/Reclamation.java
+++ src/main/java/me/haitammk/citoyenconnect/reclamation/Reclamation.java
@@ -1,5 +1,43 @@
 package me.haitammk.citoyenconnect.reclamation;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Table
+@Entity(name = "reclamation")
 public class Reclamation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_reclamation")
+    private Long id;
     
+    @NotBlank
+    @Column(name = "nom")
+    private String nom;
+
+    @NotBlank
+    @Email
+    @Column(name = "email")
+    private String email;
+
+    @NotBlank
+    @Column(name = "message")
+    private String message;
 }
