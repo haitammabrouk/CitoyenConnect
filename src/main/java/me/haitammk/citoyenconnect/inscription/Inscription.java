3767f21e44190238766a42d4da4e868524a6619b
haitammabrouk <haitammk0708@gmail.com>
2023-12-19 11:48:54 +0100

    create necessary folders (admin, arrondisement, personne, demandes, inscription, reclamation ...)

---
+++ src/main/java/me/haitammk/citoyenconnect/inscription/Inscription.java
@@ -0,0 +1,11 @@
+package me.haitammk.citoyenconnect.inscription;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "inscription")
+public class Inscription {
+    
+    
+}
