1b1eab0ac2d099c6dfa0c4b8d811f8e7f6dcf71a
haitammabrouk <haitammk0708@gmail.com>
2023-12-22 18:04:10 +0100

    set inscription cas de rejet

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import FonctionnaireArronDemandeInsc from './pages/FonctionnaireArronDemandeInsc
 import FonctionnaireDemandeInscDetails from './pages/FonctionnaireDemandeInscDetails';
 import Reclamation from './pages/Reclamation';
 import CitoyenDemandes from './pages/CitoyenDemandes';
+import DemandeInscription from './pages/DemandeInscription';
+import FonctionnaireArronReclamations from './pages/FonctionnaireArronReclamations'
 
 
 
@@ -20,7 +22,9 @@ function App() {
           <Route path='/inscription-en-cours' element={<InscriptionEnCours/>} />
           <Route path='/fonctionnaire-demandes-inscription' element={<FonctionnaireArronDemandeInsc/>} />
           <Route path='/fonctionnaire-demandes-inscription-details/:id' element={<FonctionnaireDemandeInscDetails/>} />
+          <Route path='/fonctionnaire-reclamations' element={<FonctionnaireArronReclamations/>} />
           <Route path='/citoyen-demandes' element={<CitoyenDemandes/>} />
+          <Route path='/demande-inscription' element={<DemandeInscription/>} />
         </Routes>
       </Router>
     </>
--- src/components/DemandeInscriptionCardDetails.jsx
+++ src/components/DemandeInscriptionCardDetails.jsx
@@ -1,18 +1,46 @@
 import axios from 'axios';
+import { useState } from 'react';
 import { Link } from 'react-router-dom'
 
 // eslint-disable-next-line react/prop-types
 function DemandeInscriptionCardDetails({ inscription, nom, id, date }) {
 
+    const [raison, setRaison] = useState('');
+    const [checkAccept, setCheckAccept] = useState(false);
+    const [checkRejet, setCheckRejet]= useState(false);
+
+    const handleRaisonChange = (e) => {
+        setRaison(e.target.value);
+    }
+
+    const setRejeter = (e) => {
+        e.preventDefault()
+        axios.post(`http://localhost:8080/inscription-rejeter`, null, {
+        params: {
+            id: id,
+            raison:raison
+        }
+        })
+        .then((response) => {
+            console.log("rapport est rejete");
+            setRaison('');
+            setCheckRejet(true)
+            
+        }).catch((error) => {
+            console.log("rapport n'est pas rejete");
+        })
+    }
+
     const setAccepter = (e) => {
         e.preventDefault()
         axios.post(`http://localhost:8080/inscription-accepter`, null, {
         params: {
-            id: id
+            id: id,
         }
         })
         .then((response) => {
             console.log("rapport est accepte");
+            setCheckAccept(true);
             
         }).catch((error) => {
             console.log("rapport n'est pas accepte");
@@ -36,12 +64,18 @@ function DemandeInscriptionCardDetails({ inscription, nom, id, date }) {
                     <ul className='text-md text-[#636363] font-semibold  flex justify-around space-x-3'>
                         <li className='border-r border-r-[#000000]'><p>Pour accepter la demande, sélectionnez 'Accepter la Demande'</p>
                         <button onClick={setAccepter} className='py-2 px-4 mt-2 bg-[#58815c] text-[#ffffff]'>Accepter La Demande</button>
+                        {
+                            checkAccept && <p className=" pt-2 font-bold text-sm text-[#2b5b41]">Le demande inscription a ete accepte.</p>
+                        }
                         </li>
                         <li className='pl-2'><p>Pour rejeter la demande, spécifiez la raison du rejet et sélectionnez 'Rejeter la Demande'</p>
-                        <form action="" method='post'>
-                            <textarea className='h-44 w-full mt-4 outline-none border border-[#636363] text-[#636363] px-2 py-2 rounded-lg' name="raison" id="raison" cols="30" rows="10" placeholder='la raison du rejet'></textarea>
+                        <form onSubmit={setRejeter} method='post'>
+                            <textarea onChange={handleRaisonChange} value={raison} className='h-44 w-full mt-4 outline-none border border-[#636363] text-[#636363] px-2 py-2 rounded-lg' name="raison" id="raison" cols="30" rows="10" placeholder='la raison du rejet'></textarea>
                             <button className='py-2 px-4 mt-2 bg-[#86433b] text-[#ffffff]'>Rejeter La Demande</button>
                         </form>
+                        {
+                            checkRejet && <p className=" pt-2 font-bold text-sm text-[#86433b]">Le demande inscription a ete rejete.</p>
+                        }
                         </li>
                     </ul>
                 </div>
